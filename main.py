bd8cd139b36bd292902c0d3ca8aada703c24f2d5
Claquettes <math@claq.fr>
2023-02-11 21:50:57 +0100

    mon dieu mais  que de ratios

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+import string  
+import random
+# on importe le fichier ratio.txt, qui est dans le même dossier que main.py, sachant qu'on sépare le contenu du fichier à chaque caractèrte "+"
+with open("ratio.txt", "r") as f:
+    ratio = f.read().split("+")
+
+#on génère une chaine de caractère aléatoire contenant 15 des mots de ratio.txt
+def ratioUltime():
+    ratioUltime = ""
+    for i in range(15):
+        ratioUltime += random.choice(ratio) + "+"
+    return ratioUltime
+
+print('ratio +' +  ratioUltime())
+
+
+
