5e5d8c7e286b7074a4a30d67718950210b71d918
Claquettes <math@claq.fr>
2023-02-12 15:45:36 +0100

    Gui! now the programm opens a windows

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 import string  
 import random
 # on importe le fichier ratio.txt, qui est dans le même dossier que main.py, sachant qu'on sépare le contenu du fichier à chaque caractèrte "+"
@@ -11,7 +13,18 @@ def ratioUltime():
         ratioUltime += random.choice(ratio) + "+"
     return ratioUltime
 
-print('Déguste moi ce délicieux ratio ' +  ratioUltime() + 'polymanu is better than you')
+##print('Déguste moi ce délicieux ratio ' +  ratioUltime() + 'polymanu is better than you')
 
+# on crée la fenêtre
+window = Tk()
+window.title("Ratio Ultime")
+window.geometry("200x500")
 
+button = Button(window, text="Générer un ratio", command=ratioUltime).pack()
+button2 = Button(window, text="Quitter", command=window.quit).pack()
 
+label = Label(window, text=ratioUltime(), wraplength=100).pack()
+#on ajoute un bouton pour coier le ratio dans le presse papier de l'utilisateur, sous forme de texte
+button3 = Button(window, text="Copier le ratio", command=window.clipboard_append(ratioUltime())).pack()
+
+window.mainloop()
